95ca204e5dd4b7db17a2c73d1cbd35e77fe63834
Tom Ashworth <thomas.d.ashworth@gmail.com>
2023-02-02 19:37:04 +0000

    refactor: relocate internal resources

--- middleware.ts
+++ middleware.ts
@@ -19,7 +19,7 @@ type SiteConfig = {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|favicon.ico|global.css).*)'],
+  matcher: ['/((?!_next/static|favicon.ico).*)'],
 }
 
 export async function middleware(request: NextRequest) {
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app'
-import '../global.css'
+import '../public/_internal/global.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
